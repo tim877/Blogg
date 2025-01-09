81959d178a5e0669b572a56f1fa2c3b878961083
tim899 <timhagman87@gmail.com>
2025-01-09 21:47:14 +0100

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'blogg-project' title`, () => {
+  it(`should have the 'blogg' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blogg-project');
+    expect(app.title).toEqual('blogg');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, blogg-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, blogg');
   });
 });
